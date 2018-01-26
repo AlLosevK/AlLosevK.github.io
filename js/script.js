6b21ac58e0fb794e0d5fd937dc340d295a00c6b1
AlLosevK <al.losev.k@gmail.com>
2018-01-26 18:59:40 +0300

    New design. Header

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.container').on('click', function () {
-
-        if ($('.project-link').hasClass("non-active")) {
-            $('.attention').fadeIn(10);
-        }
-    })
-})
