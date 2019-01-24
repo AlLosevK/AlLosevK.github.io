459e2d7396b940c0164d9ab594dd70d432b60e8d
Aleksandr <al.losev.k@gmail.com>
2019-01-24 14:02:12 +0300

    Delete old files

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,16 @@
 $('.review-slider').slick({
     dots: true,
     infinite: true,
-    speed: 3000,
+    speed: 1000,
     slidesToShow: 1,
     autoplay: true,
+    arrows: false,
     autoplaySpeed: 2000,
     adaptiveHeight: true
 });
+
 $('.portfolio-slider').slick({
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 1500,
     slidesToShow: 1,
@@ -16,8 +18,7 @@ $('.portfolio-slider').slick({
     arrows: false,
     asNavFor: '.portfolio__descr-slider',
     autoplay: true,
-    autoplaySpeed: 2000,
-    appendDots: '.dots'
+    autoplaySpeed: 2000
 });
 
 $('.portfolio__descr-slider').slick({
@@ -29,4 +30,20 @@ $('.portfolio__descr-slider').slick({
     asNavFor: '.portfolio-slider',
     dots: false,
     draggable: false
+});
+
+$(document).ready(function (){
+  $(".dots__item").on("click", function(){
+    var data_number = $(this).attr('data-number')
+    $(".portfolio-slider").slick('slickGoTo', data_number)
+  });
+});
+
+$(document).ready(function (){
+  $(".main-content__btn.main-content__btn-mob").on("click", function(){
+    $(".main-content__background-mob, .main-content__form").addClass("is-active");
+  });
+  $(".main-content__background-mob").on("click", function(){
+    $(".main-content__background-mob, .main-content__form").removeClass("is-active");
+  });
 });
