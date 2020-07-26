2f53ff1fac3406b2ed8915358fb42ab940992a3a
AlLosevK <al.losev.k@gmail.com>
2020-07-26 14:55:58 +0300

    Final 80%

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
+import Swiper from '../local_modules/swiper/swiper-bundle.min.js';
 
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
